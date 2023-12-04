16c8631cb65e1f39df98ee412b6bdbe069fef43e
Steve Steinitz <steve@stzdev.com>
2023-12-04 17:56:06 +1100

    Get Gas price in AUD from CoinMarketCap

--- hardhat.config.js
+++ hardhat.config.js
@@ -2,6 +2,7 @@ require("@nomicfoundation/hardhat-toolbox")
 // included in the above - require("@nomiclabs/hardhat-etherscan")
 require("dotenv").config()
 require('./tasks/block-number')
+require('hardhat-gas-reporter')
 
 const {
   SEPOLIA_CHAIN_ID,
@@ -10,7 +11,8 @@ const {
 const SEPOLIA_RPC_URL = process.env.SEPOLIA_RPC_URL
 const SEPOLIA_KEY = process.env.SEPOLIA_KEY
 const ETHERSCAN_API_KEY = process.env.ETHERSCAN_API_KEY
-
+const COINMARKETCAP_API_KEY = process.env.COINMARKETCAP_API_KEY
+console.log({COINMARKETCAP_API_KEY})
 /** @type import('hardhat/config').HardhatUserConfig */
 module.exports = {
   defaultNetwork: "hardhat", // not needed but more explicit
@@ -30,6 +32,14 @@ module.exports = {
   etherscan: {
     apiKey: ETHERSCAN_API_KEY,
   },
+  gasReporter: {
+    enabled: true,
+    outputFile: 'gas-report.txt',
+    noColors: true,
+    currency: 'AUD',
+    coinmarketcap: COINMARKETCAP_API_KEY,
+  }
+ 
 }
 
 // task("accounts", "Prints the list of accounts", async () => {
